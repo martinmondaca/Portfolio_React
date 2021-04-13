316c7e082c34b4918f10d4376a15745e1684a2f9
Martin M <martin7mondaca@gmail.com>
2021-04-12 23:38:50 -0700

    added router, navbar, wrapper components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import Navbar from "./components/Navbar"
+import Wrapper from "./components/Wrapper"
+import Project from "./components/Project"
+import About from "./pages/About"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Router>
+          <div>
+            <Navbar />
+            <Wrapper>
+              <Route exact path="/about" component={About} />
+              <Route exact path="/portfolio" component={Project} />
+            </Wrapper>
+          </div>
+        </Router>
+      </div>    </>
   );
 }
 
