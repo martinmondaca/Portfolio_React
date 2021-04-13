0ecbe547a82c6d98207670b6e2aa852e1424b1dd
Martin M <martin7mondaca@gmail.com>
2021-04-12 21:04:33 -0700

    added basic about me page

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function About() {
+    return (
+        <div className="container">
+            <h3>About Me</h3>
+            <p>I'm a current full-stack web development student at the University of Arizona coding bootcamp.
+            Expected to complete the boot camp in May 2021. I'm currently in the market for a job in web development;
+            more specifically work that involved front-end web development, database administration, or React.js.
+            </p>
+            <p>I'm an Arizona native with a passion for coding, science, and video games.</p>
+            <p></p>
+        </div>
+    )
+}
+
+export default About;
