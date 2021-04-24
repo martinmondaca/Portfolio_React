76c322e81b50e6cf018998b8a25b416f51a959f6
Martin M <martin7mondaca@gmail.com>
2021-04-24 03:52:39 -0700

    cleaned up layout of tags

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,51 +1,66 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Pdf from "../../assets/resume2020.pdf"
-// import "./style.css";
+// import Pdf from "../../assets/resume2020.pdf"
+import "./style.css";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link className="navbar-brand" to="/">
-                Martin Mondaca
-      </Link>
-            <div>
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <Link
-                            to="/about"
-                            className={window.location.pathname === "/" || window.location.pathname === "/about"
-                                ? "nav-link active"
-                                : "nav-link"
-                            }
-                        >
-                            About
-            </Link>
+
+    return (<>
+
+        <nav className="light-blue lighten-5">
+            <div className="nav-wrapper">
+                <Link className="brand-logo center" to="/" >
+                    <span className="black-text text-black">Martin Mondaca</span>
+                </Link>
+                <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons black-text text-black">menu</i></a>
+                <ul className="left hide-on-med-and-down black-text">
+                    <li>
+                        <Link to="/">
+                            <span className="black-text text-black">About Me</span>
+                        </Link>
                     </li>
-                    <li className="nav-item">
-                        <Link
-                            to="/portfolio"
-                            className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
-                        >
-                            Portfolio
-            </Link>
+                    <li>
+                        <Link to="/portfolio" >
+                            <span className="black-text text-black">My Work</span>
+                        </Link>
                     </li>
-                    <li className="nav-item">
-
-                        <a href="mailto:collectemall@gmail.com" target="_blank" rel="noreferrer">Contact</a>
-
-
+                    <li>
+                        <a className="black-text text-black" href="mailto:collectemall@gmail.com" target="_blank" rel="noreferrer">Email Me</a>
                     </li>
-
-                    <li className="nav-item">
-
-                        <a href={Pdf} target="_blank" rel="noreferrer">Resume</a>
-
+                    <li>
+                        <Link to="/resume">
+                            <span className="black-text text-black">Resume</span>
+                        </Link>
                     </li>
                 </ul>
             </div>
-        </nav >
+        </nav>
+
+        <ul className="sidenav" id="mobile-demo">
+            <li >
+                <Link to="/">
+                    About Me
+                </Link>
+            </li>
+            <li>
+                <Link to="/portfolio">
+                    My Work
+                </Link>
+            </li>
+            <li>
+                <a href="mailto:collectemall@gmail.com" target="_blank" rel="noreferrer">Email Me</a>
+            </li>
+            <li>
+                <Link to="/resume">
+                    Resume
+                </Link>
+            </li>
+        </ul>
+
+
+    </>
+
     );
 }
 
